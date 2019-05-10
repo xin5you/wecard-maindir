64fbdbd1b6eeeced10993174f22c475771528fdd
liuxl <liuxl1@fosun.com>
2019-05-10 15:03:55 +0800

    添加wxp启动加载公众号信息日志

--- wecard-customer/src/main/java/com/cn/thinkx/wecard/customer/module/wxcms/AppDefineInitService.java
+++ wecard-customer/src/main/java/com/cn/thinkx/wecard/customer/module/wxcms/AppDefineInitService.java
@@ -6,8 +6,6 @@ import com.cn.thinkx.wecard.customer.core.spring.SpringBeanDefineService;
 import com.cn.thinkx.wecard.customer.module.wxcms.service.AccountService;
 import com.cn.thinkx.wechat.base.wxapi.domain.Account;
 import com.cn.thinkx.wechat.base.wxapi.process.WxMemoryCacheClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import redis.clients.jedis.JedisCluster;
@@ -16,8 +14,6 @@ import redis.clients.jedis.JedisCluster;
  * 系统启动时自动加载，把公众号信息加入到缓存中
  */
 public class AppDefineInitService implements SpringBeanDefineService {
-    private Logger logger = LoggerFactory.getLogger(getClass());
-
     @Autowired
     @Qualifier("accountService")
     private AccountService accountService;
@@ -31,7 +27,7 @@ public class AppDefineInitService implements SpringBeanDefineService {
         Account account = accountService.getByAccount(
 //				jedisCluster.hget(RedisConstants.REDIS_HASH_TABLE_TB_BASE_DICT_KV, "WX_CUSTOMER_ACCOUNT"));
                 jedisCluster.hget(RedisConstants.REDIS_HASH_TABLE_TB_BASE_DICT_KV, "WX_HUIKABAO_LIFE_ACCOUNT"));
-        logger.info("加载公众号信息：{}", JSONObject.toJSONString(account));
+        System.out.println("加载公众号信息：" + JSONObject.toJSONString(account));
         WxMemoryCacheClient.addMpAccount(account);
     }
 
