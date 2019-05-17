75df8599aa1a640ab0d734baf31e047ecc8e3d5b
zhuqiuyou <zhuqiuyou_zqy@163.com>
2019-05-17 19:43:47 +0800

    中付代付接口

--- common/common-pay-core/src/main/java/com/cn/thinkx/pay/core/KeyUtils.java
+++ common/common-pay-core/src/main/java/com/cn/thinkx/pay/core/KeyUtils.java
@@ -2,6 +2,7 @@ package com.cn.thinkx.pay.core;
 
 import com.alibaba.fastjson.JSONObject;
 import com.cn.thinkx.pms.base.http.HttpClientUtil;
+import com.cn.thinkx.pms.base.redis.util.RedisDictProperties;
 import com.cn.thinkx.pms.base.utils.RandomUtils;
 
 import java.text.SimpleDateFormat;
@@ -16,20 +17,22 @@ import java.util.Map;
 public class KeyUtils {
     
 	//测试数据
-    private static final String URL="http://test2.guangyinwangluo.com:9999/swPayInterface/key/";//测试
-	//private static final String URL="http://paygw.guangyinwangluo.com/swPayInterface/key/";   //生产
-    private static final String IP="127.0.0.1";
-    private static final String USER_KEY="990149635210001";
-    private static final String ORGAN_NO="990";
+	public static final String ZHONGFU_PAY_URL="ZHONGFU_PAY_URL"; //中付获取keyURL路径
+	public static final String ZHONGFU_KEY_URL="ZHONGFU_KEY_URL"; //中付获取keyURL路径
+	public static final String ZHONGFU_PAY_USER_KEY="ZHONGFU_PAY_USER_KEY"; //中付商户号
+	public static final String ZHONGFU_ORGAN_NO="ZHONGFU_ORGAN_NO";  //中付结构号
+	public static final String ZHONGFU_PAY_KEY="ZHONGFU_PAY_KEY";  //中付代付密码
+
     private static final String PAY_TYPE="73";
+	private static final String IP="127.0.0.1";
     public static final String responseCode="00";  //代付成功标识
 
 	public  String getRSAKey(){
 		String batchNo =new SimpleDateFormat("yyyyMMdd").format(new Date());
 		String ip = IP;
 		String serialNo = RandomUtils.getRandomNumbernStr(10)+new Date().getTime();//0123456789应随机生成
-		String userKey = USER_KEY;
-		String organNo=ORGAN_NO;
+		String userKey = RedisDictProperties.getInstance().getdictValueByCode(ZHONGFU_PAY_USER_KEY);
+		String organNo=RedisDictProperties.getInstance().getdictValueByCode(ZHONGFU_ORGAN_NO);
 		String payType=PAY_TYPE;
 		JSONObject json=new JSONObject();
 		json.put("batchNo", batchNo);
@@ -42,7 +45,7 @@ public class KeyUtils {
 		String res="";
 		String signature=RSAUtil.getSHA256Str(preSign);
 		json.put("signature", signature);
-		res=HttpClientUtil.sendPostReturnStr(URL+"getRSAKey", json.toString());
+		res=HttpClientUtil.sendPostReturnStr(RedisDictProperties.getInstance().getdictValueByCode(ZHONGFU_KEY_URL)+"getRSAKey", json.toString());
 		System.out.println("res="+res);
 		String privateKey=JSONObject.parseObject(res).getString("RSAPrivateKey");
 		return privateKey;
@@ -51,8 +54,8 @@ public class KeyUtils {
 		String batchNo =new SimpleDateFormat("yyyyMMdd").format(new Date());
 		String ip = IP;
 		String serialNo = RandomUtils.getRandomNumbernStr(10)+new Date().getTime();//9876543210应随机生成
-		String userKey = USER_KEY;
-		String organNo = ORGAN_NO;
+		String userKey = RedisDictProperties.getInstance().getdictValueByCode(ZHONGFU_PAY_USER_KEY);
+		String organNo=RedisDictProperties.getInstance().getdictValueByCode(ZHONGFU_ORGAN_NO);
 		String payType = PAY_TYPE;
 		JSONObject json=new JSONObject();
 		json.put("batchNo", batchNo);
@@ -65,7 +68,7 @@ public class KeyUtils {
 		String res="";
 		String signature=RSAUtil.sign(preSign.getBytes(), privateKey);
 		json.put("signature", signature);
-		res=HttpClientUtil.sendPostReturnStr(URL+"getKeys", json.toString());
+		res=HttpClientUtil.sendPostReturnStr(RedisDictProperties.getInstance().getdictValueByCode(ZHONGFU_KEY_URL)+"getKeys", json.toString());
 		String aes=JSONObject.parseObject(res).getString("AES");
 		String md5=JSONObject.parseObject(res).getString("MD5");
 		aes=decode(aes);
@@ -79,8 +82,8 @@ public class KeyUtils {
 		String batchNo =new SimpleDateFormat("yyyyMMdd").format(new Date());
 		String ip = IP;
 		String serialNo = RandomUtils.getRandomNumbernStr(10)+new Date().getTime();//9876543210应随机生成
-		String userKey = USER_KEY;
-		String organNo = ORGAN_NO;
+		String userKey = RedisDictProperties.getInstance().getdictValueByCode(ZHONGFU_PAY_USER_KEY);
+		String organNo=RedisDictProperties.getInstance().getdictValueByCode(ZHONGFU_ORGAN_NO);
 		String payType = PAY_TYPE;
 		JSONObject json=new JSONObject();
 		json.put("batchNo", batchNo);
@@ -93,7 +96,7 @@ public class KeyUtils {
 		String res="";
 		String signature=RSAUtil.sign(preSign.getBytes(), privateKey);
 		json.put("signature", signature);
-		res=HttpClientUtil.sendPostReturnStr(URL+"getZek", json.toString());
+		res=HttpClientUtil.sendPostReturnStr(RedisDictProperties.getInstance().getdictValueByCode(ZHONGFU_KEY_URL)+"getZek", json.toString());
 		String zek=JSONObject.parseObject(res).getString("ZEK");
 		zek=decode(zek);
 		Map<String, String> resMap=new HashMap<String, String>(2);
--- common/common-pay-core/src/main/java/com/cn/thinkx/pay/service/ZFPaymentServer.java
+++ common/common-pay-core/src/main/java/com/cn/thinkx/pay/service/ZFPaymentServer.java
@@ -2,26 +2,24 @@ package com.cn.thinkx.pay.service;
 
 import com.alibaba.fastjson.JSONObject;
 import com.cn.thinkx.pay.core.DES3;
-import com.cn.thinkx.pay.core.KeyDemo;
 import com.cn.thinkx.pay.core.KeyUtils;
-import com.cn.thinkx.pay.core.RSAUtil;
 import com.cn.thinkx.pay.domain.UnifyPayForAnotherVO;
 import com.cn.thinkx.pay.domain.UnifyQueryVO;
 import com.cn.thinkx.pms.base.http.HttpClientUtil;
-import com.cn.thinkx.pms.base.utils.DES;
+import com.cn.thinkx.pms.base.redis.util.RedisDictProperties;
 import com.cn.thinkx.pms.base.utils.MD5Util;
-import sun.security.krb5.internal.crypto.Des;
-import sun.security.krb5.internal.crypto.Des3;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
-import java.util.UUID;
 
 /**
  * 中付统一代付服务接口
  */
 public class ZFPaymentServer {
 
-    private static final String PAYFORANOTHER_URL="http://210.13.122.106:2008/paymentServer/PayForAnother";//统一代付接口
+    private static  Logger logger = LoggerFactory.getLogger(ZFPaymentServer.class);
+
 
     private static final String Str32="00000000000000000000000000000000";
     /**
@@ -33,25 +31,15 @@ public class ZFPaymentServer {
         KeyUtils k=new KeyUtils();
         String res="";
         try {
-            //1,先获取rsa私钥
-      /*      String privateKey=k.getRSAKey();
-            System.out.println(privateKey);
-            //2,获取所需密钥
-            //md5,aes
-            Map<String, String> map=k.getKeys(privateKey);
-            String aes=map.get("AES");
-            String md5=map.get("MD5");
-            System.out.println("AES:"+aes+",MD5:"+md5);*/
-
             JSONObject params=new JSONObject();
             params.put("service", "p4aSignIn");
             params.put("merchantNo", merchantNo);
             params.put("MD5", MD5Util.md5(merchantNo+md5).toUpperCase());
-            res=HttpClientUtil.sendPostReturnStr(PAYFORANOTHER_URL, params.toJSONString());
-           System.out.println("返回的签到数据："+res);
+            res=HttpClientUtil.sendPostReturnStr(RedisDictProperties.getInstance().getdictValueByCode(KeyUtils.ZHONGFU_PAY_URL), params.toJSONString());
+            logger.info("返回的签到数据：{}",res);
 
         }catch (Exception ex){
-            ex.printStackTrace();
+            logger.error("##代付签到接口执行异常{}", ex);
         }
         return JSONObject.parseObject(res);
     }
@@ -67,39 +55,19 @@ public class ZFPaymentServer {
         try {
             //1,先获取rsa私钥
             String privateKey=k.getRSAKey();
+
             //2,获取所需密钥
             //md5,aes
             Map<String, String> map=k.getKeys(privateKey);
             String aes=map.get("AES");
             String MD5=map.get("MD5");
-            System.out.println("aes:"+aes);
-            System.out.println("MD5:"+MD5);
 
             //zek
             Map<String, String> map2=k.getZek(privateKey);
             String zek=map2.get("ZEK");
-            System.out.println("ZEK:"+zek);
 
             System.out.println(zek.length());
             JSONObject msg=new JSONObject();
-           /* params.put("service", un.getService());
-            params.put("merchantNo", un.getMerchantNo());
-            params.put("payMoney", DES3.encrypt3DES((Str32.substring(0, 32-un.getPayMoney().length())+un.getPayMoney()),zek));
-            params.put("orderId", un.getOrderId());
-            params.put("bankCard", DES3.encrypt3DES((Str32.substring(0, 32-un.getBankCard().length())+un.getBankCard()),zek));
-            params.put("name", un.getName());
-            params.put("bankName", un.getBankName());
-            params.put("acctType", un.getAcctType());
-            params.put("cnaps", un.getCnaps());
-            params.put("province", un.getProvince());
-            params.put("city", un.getCity());
-            params.put("certType", un.getCertType());
-            params.put("certNumber", DES3.encrypt3DES((Str32.substring(0, 32-un.getCertNumber().length())+un.getCertNumber()),zek));
-            params.put("sessionId", un.getSessionId());
-            params.put("payKey", un.getPayKey());
-            String md5Str=un.getOrderId()+un.getMerchantNo()+un.getPayMoney()+un.getBankCard()+MD5;
-            System.out.println(md5Str);
-            params.put("MD5", MD5Util.md5(md5Str));*/
 
             msg.put("merchantNo", un.getMerchantNo());
             msg.put("service", "p4aPay");
@@ -117,12 +85,17 @@ public class ZFPaymentServer {
             msg.put("sessionId", un.getSessionId());
             msg.put("payKey", un.getPayKey());
 
+            //orderId+merchantNo+payMoney+bankCard+key(MD5校验,参数均为明文进行加密)
+            //MD5密钥参见3.2.3获取MD5密钥接口
             msg.put("MD5", MD5Util.md5(msg.getString("orderId")+msg.getString("merchantNo")+un.getPayMoney()+un.getBankCard()+MD5).toUpperCase());
 
-            res=HttpClientUtil.sendPostReturnStr(PAYFORANOTHER_URL, msg.toJSONString());
-            System.out.println("统一代付返回的结果："+res);
+            logger.info("##代付查询request params {}",msg.toJSONString());
+
+            res=HttpClientUtil.sendPostReturnStr(RedisDictProperties.getInstance().getdictValueByCode(KeyUtils.ZHONGFU_PAY_URL), msg.toJSONString());
+
+            logger.info("##中付统一代付结果 response result {}",res);
         }catch (Exception ex){
-            ex.printStackTrace();
+            logger.error("##代付接口执行异常{}", ex);
         }
         return JSONObject.parseObject(res);
     }
@@ -145,18 +118,11 @@ public class ZFPaymentServer {
             params.put("tradeTime", queryVO.getTradeTime());
             params.put("sessionId", queryVO.getSessionId());
             params.put("MD5", MD5Util.md5((params.getString("orderNumber")+params.getString("merchantNo")+queryVO.getMD5())).toUpperCase());
-
-      /*      msg.put("service", "p4aQuery");
-            msg.put("merchantNo", queryVO.getMerchantNo());
-            msg.put("sessionId", queryVO.getSessionId());
-            msg.put("orderNumber",  queryVO.getOrderNumber());
-            msg.put("tradeTime", "2019-05-17");
-            msg.put("MD5", MD5Util.md5(msg.getString("orderNumber")+msg.getString("merchantNo")+MD5Key,"UTF-8"));*/
-
-            res=HttpClientUtil.sendPostReturnStr(PAYFORANOTHER_URL, params.toJSONString());
-            System.out.println("代付查询返回的结果："+res);
+            logger.info("##代付查询request params {}",params.toJSONString());
+            res=HttpClientUtil.sendPostReturnStr(RedisDictProperties.getInstance().getdictValueByCode(KeyUtils.ZHONGFU_PAY_URL), params.toJSONString());
+            logger.info("##代付查询response result {}",res);
         }catch (Exception ex){
-            ex.printStackTrace();
+            logger.error("##代付查询接口执行异常{}", ex);
         }
         return JSONObject.parseObject(res);
     }
