0e98b8ae2451758a1fe1104b0f5cc9a8ee45625c
zhuqiuyou <zhuqiuyou_zqy@163.com>
2019-06-10 18:49:32 +0800

    代付交易 和 查询接口

---
+++ common/common-pay-core/src/main/java/com/cn/thinkx/cgb/model/QueryAccountBalDTO.java
@@ -0,0 +1,12 @@
+package com.cn.thinkx.cgb.model;
+
+import java.io.Serializable;
+
+/**
+ * 查询账户余额
+ */
+public class QueryAccountBalDTO implements Serializable {
+
+    private static final long serialVersionUID = 7606634695832137333L;
+
+}
--- common/common-pay-core/src/main/java/com/cn/thinkx/cgb/service/CgbService.java
+++ common/common-pay-core/src/main/java/com/cn/thinkx/cgb/service/CgbService.java
@@ -8,6 +8,9 @@ import java.text.SimpleDateFormat;
 
 public class CgbService {
 
+    /**
+     * 代付请求
+     */
     public void dfPaymentResult() {
         CgbInit cgbInit = new CgbInit();
         CommHeadDTO commHeadDTO = new CommHeadDTO();
@@ -48,8 +51,44 @@ public class CgbService {
 
     }
 
-
+    /**
+     * 查询订单状态
+     */
     public void dfQueryOrderResult() {
+        CgbInit cgbInit = new CgbInit();
+        CommHeadDTO commHeadDTO = new CommHeadDTO();
+        commHeadDTO.setTranCode("1004");
+        commHeadDTO.setCifMaster(cgbInit.getMaster());//
+        commHeadDTO.setEntSeqNo("800000001200"+"102");
+        commHeadDTO.setEntUserId(cgbInit.getUserId());
+        commHeadDTO.setPassword(cgbInit.getPassword());
+        Timestamp time = new Timestamp(System.currentTimeMillis());
+        commHeadDTO.setTranDate(new SimpleDateFormat("yyyyMMdd").format(time));
+        commHeadDTO.setTranTime(new SimpleDateFormat("HHmmss").format(time));
+        commHeadDTO.setRetCode("000");
+
+        QueryPaymentDTO queryPaymentDTO=new QueryPaymentDTO();
+        queryPaymentDTO.setOrigEntseqno("800000001200102");
+        queryPaymentDTO.setOrigEntdate("20190610");
+        System.out.println(("queryPaymentDTO {}" + queryPaymentDTO));
+        RequestParametersDTO<QueryPaymentDTO> requestParametersDTO = new RequestParametersDTO<QueryPaymentDTO>(queryPaymentDTO);
+        requestParametersDTO.setCommHeadDTO(commHeadDTO);
+        String strXml = CGBUtil.getxml(requestParametersDTO, cgbInit.getPubPath(), cgbInit.getPvbPath());
+        try {
+            System.out.println("请求参数 {}" + strXml);
+            String xmlMap = CGBUtil.doPost(cgbInit.getUpProt(), strXml);
+            System.out.println("返回参数：" + xmlMap);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 查询账号余额
+     */
+    public void queryAccountBalResult() {
         CgbInit cgbInit = new CgbInit();
         CommHeadDTO commHeadDTO = new CommHeadDTO();
         commHeadDTO.setTranCode("1004");
