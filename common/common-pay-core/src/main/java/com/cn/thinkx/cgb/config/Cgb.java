3a7b78143a1aa4b3c979348b39ec4a454010597f
zhuqiuyou <zhuqiuyou_zqy@163.com>
2019-06-10 17:36:03 +0800

    代付交易 和 查询接口

--- common/common-pay-core/src/main/java/com/cn/thinkx/cgb/config/Cgb.java
+++ common/common-pay-core/src/main/java/com/cn/thinkx/cgb/config/Cgb.java
@@ -9,6 +9,6 @@ import com.cn.thinkx.cgb.service.CgbService;
  */
 public class Cgb {
     public static void main(String[] args) {
-        new CgbService().dfPaymentResult();
+        new CgbService().dfQueryOrderResult();
     }
 }
